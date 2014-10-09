cfa76a95605b176c098d5885cc18292561c12849
Dmitry Panteleev <dpantele@users.noreply.github.com>
2014-10-08 23:22:45 -0400

    Added information about conjugators used

--- FoldedGraphAccHarvest/main/chains.cpp
+++ FoldedGraphAccHarvest/main/chains.cpp
@@ -13,6 +13,11 @@ char LabelToChar(Label l) {
   return l % 2 ? 'X' + l / 2 : 'x' + l/2;
 }
 
+template<typename U, typename V>
+std::pair<V, U> Swapped(std::pair<U, V> p) {
+  return std::pair<V, U>(std::move(p.second), std::move(p.first));
+}
+
 int main(int argc, const char *argv[]) {
   std::string prefix = "h14_c3";
   std::ifstream generated_words_in(prefix + "_unproc_words.txt");
@@ -92,7 +97,7 @@ int main(int argc, const char *argv[]) {
       PrintWord(images[l], &std::cout);
       std::cout << "$, ";
     }
-    std::cout << "\n\n";
+    std::cout << "\n";
 
     auto TransformWord = [&images](Word w) -> Word {
       Word result;
@@ -114,23 +119,53 @@ int main(int argc, const char *argv[]) {
       std::cout << " missing" << std::endl;
       continue;
     }
-    std::cout << "\\[\\begin{split}\n";
+    std::cout << "\\begin{align*}\n";
     while (next->second != 0) {
-      std::cout << "\\langle";
+      std::cout << "\\,&\\langle";
       PrintTo(current_words.first, &std::cout);
       std::cout << " \\mid ";
       PrintTo(current_words.second, &std::cout);
-      std::cout << "\\rangle\\\\ \n&\\leftarrow ";
+      std::cout << "\\rangle";
       auto next_index = next->second;
+      auto previous_words = current_words;
       current_words = generating_words[next_index];
+
+      if (Swapped(current_words) == previous_words) {
+        std::cout << "\\;&\\text{swapped}";
+      } else {
+        FoldedGraph2 g;
+        g.PushCycle(current_words.first, g.root(), 1);
+        for (auto i = 0u; i < 3; ++i) {
+          g.CompleteWith(current_words.second);
+        }
+
+        for (auto v = g.root(); v < g.size(); ++v) {
+          auto v_harvest = g.Harvest(previous_words.second.size(), v, v, 1);
+          for (auto&& u : v_harvest) {
+            if (GetCanonicalPair(current_words.second, u) == previous_words) {
+              auto conjugator = g.GetPathFromRoot(v);
+              if (conjugator.Empty()) {
+                std::cout << "\\;&\\text{no conjugator}";
+              } else {
+                std::cout << "\\;&c = ";
+              }
+              PrintWord(conjugator, &std::cout);
+              v = g.size();
+              break;
+            }
+          }
+        }
+      }
+
+      std::cout << "\\\\\n\\leftarrow";
       next = generated_words.find(current_words);
     }
-    std::cout << "\\langle";
+    std::cout << "\\,&\\langle";
     PrintTo(current_words.first, &std::cout);
     std::cout << " \\mid ";
     PrintTo(current_words.second, &std::cout);
     std::cout << "\\rangle\n";
-    std::cout << "\\end{split}\\]\n\n";
+    std::cout << "\\end{align*}\n\n";
   }
   return 0;
 }

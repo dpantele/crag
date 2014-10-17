c3ce2ea2d4b6d154811ba754c0dc96b47fddfa46
Dmitry Panteleev <dpantele@example.com>
2014-10-17 14:45:04 -0400

    Now everything work in multithread

--- FoldedGraphAccHarvest/main/main.cpp
+++ FoldedGraphAccHarvest/main/main.cpp
@@ -2,14 +2,19 @@
 
 #include <bitset>
 #include <chrono>
+#include <condition_variable>
 #include <cstdlib>
 #include <deque>
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <future>
 #include <list>
+#include <mutex>
 #include <sstream>
 #include <string>
+#include <thread>
+#include <queue>
 
 using namespace crag;
 
@@ -44,6 +49,18 @@ struct Stopwatch {
     Iteration(Stopwatch* stopwatch)
       : stopwatch_(stopwatch)
     { }
+
+    Iteration(const Iteration& other) = delete;
+
+    Iteration(Iteration&& other)
+      : stopwatch_(other.stopwatch_)
+      , duration_(other.duration_)
+      , last_click_(other.last_click_)
+      , clicks_count_(other.clicks_count_)
+    { 
+      other.clicks_count_ = 0;
+      other.duration_ = other.duration_.zero();
+    }
 
     inline bool IsRunning() const {
       return clicks_count_ % 2 == 1;
@@ -111,78 +128,85 @@ std::pair<V, U> Swapped(std::pair<U, V> p) {
 
 struct Stopwatches {
   typedef Stopwatch Field;
-  std::array<Field, 4> data_;
+  Field folding_;
+  Field harvest_;
+  Field normalize_;
+  Field reweight_;
 
   Field& folding() {
-    return data_[0];
+    return folding_;
   }
   Field& harvest() {
-    return data_[1];
+    return harvest_;
   }
   Field& normalize() {
-    return data_[2];
+    return normalize_;
   }
   Field& reweight() {
-    return data_[3];
+    return reweight_;
   }
+
+  Stopwatches() { }
+
+  //non-movable, non-copyable
+  Stopwatches(const Stopwatches&) = delete;
+  Stopwatches(Stopwatches&&) = delete;
+  Stopwatches& operator=(const Stopwatches&) = delete;
+  Stopwatches& operator=(Stopwatches&&) = delete;
 
   long long total() const {
     long long total = 0;
-    for (auto&& timer : data_) {
-      total += timer.total_time();
-    }
+    total += folding_.total_time();
+    total += harvest_.total_time();
+    total += normalize_.total_time();
+    total += reweight_.total_time();
     return total;
   }
 };
 
-
 struct StopwatchesIteration {
   typedef Stopwatch::Iteration Field;
-  std::array<Field, 4> data_;
+  Field folding_;
+  Field harvest_;
+  Field normalize_;
+  Field reweight_;
 
   Field& folding() {
-    return data_[0];
+    return folding_;
   }
   Field& harvest() {
-    return data_[1];
+    return harvest_;
   }
   Field& normalize() {
-    return data_[2];
+    return normalize_;
   }
   Field& reweight() {
-    return data_[3];
+    return reweight_;
   }
 
   StopwatchesIteration(Stopwatches* s)
-    : data_({s->data_[0].NewIter(), s->data_[1].NewIter(), s->data_[2].NewIter(), s->data_[3].NewIter()})
+    : folding_(s->folding().NewIter())
+    , harvest_(s->harvest().NewIter())
+    , normalize_(s->normalize().NewIter())
+    , reweight_(s->reweight().NewIter())
   { }
+
+  StopwatchesIteration(const StopwatchesIteration&) = delete;
+  StopwatchesIteration(StopwatchesIteration&&) = delete;
+  StopwatchesIteration& operator=(const StopwatchesIteration&) = delete;
+  StopwatchesIteration& operator=(StopwatchesIteration&&) = delete;
 
   void Reset() {
-    for (auto&& timer : data_) {
-      timer.Reset();
-    }
+    folding_.Reset();
+    harvest_.Reset();
+    normalize_.Reset();
+    reweight_.Reset();
   }
-};
-
-struct Result {
-  Word u_;
-  Word v_;
-
-  StopwatchesIteration time_;
-
-  std::vector<std::pair<Word, Word>> new_pairs_;
-
-  std::ostringstream estats_out_;
-
-  Result(Word u, Word v, Stopwatches* s)
-    : u_(std::move(u))
-    , v_(std::move(v))
-    , time_(s)
-  { }
 };
 
 struct Parameters {
   size_t max_harvest_length = Word::kMaxLength;
+  size_t workers_count_ = 1;
   std::vector<uint16_t> complete_count;
   std::pair<std::string, std::string> initial_strings;
   std::pair<std::string, std::string> required_strings;
@@ -192,47 +216,210 @@ struct Parameters {
     , initial_strings("xyxYXY", "xxxYYYY")
     , required_strings("x", "y")
   { }
-
 };
 
-void ProceedPair(const Word& u, const Word& v, const Parameters& p, Result* result) {
-  auto& time = result->time_;
+class PairToProcess {
+ public:
+  PairToProcess(Word u, Word v, const Parameters& p, Stopwatches* timer) 
+    : task_id_(GetNextId())
+    , u_(std::move(u))
+    , v_(std::move(v))
+    , p_(p)
+    , time_(timer)
+  { }
 
-  time.folding().Click();
-  FoldedGraph2 g;
-  g.PushCycle(u, g.root(), 1);
-
-  for (auto i = 0u; i < p.complete_count[v.size()]; ++i) {
-    g.CompleteWith(v);
-  }
-  time.folding().Click();
-
-  result->estats_out_ << g.size() << ", ";
-  result->estats_out_ << g.CountNontrivialEdges() << ", ";
-
-  time.reweight().Click();
-  g.Reweight();
-  time.reweight().Click();
-
-  result->estats_out_ << g.CountNontrivialEdges() << ", ";
-
-  time.harvest().Click();
-  auto eq_u = g.Harvest(p.max_harvest_length, g.root());
-  time.harvest().Click();
-
-  result->estats_out_ << eq_u.size() << ", ";
-
-  for (auto u_p = eq_u.begin(); u_p != eq_u.end(); ++u_p) {
-    time.normalize().Click();
-    auto new_pair = GetCanonicalPair(v, *u_p);
-    time.normalize().Click();
-    result->new_pairs_.push_back(std::move(new_pair));  
+  PairToProcess(const PairToProcess&) = delete;
+  PairToProcess(PairToProcess&&) = delete;
+  PairToProcess& operator=(const PairToProcess&) = delete;
+  PairToProcess& operator=(PairToProcess&&) = delete;
+  
+  const Word& u() const {
+    return u_;
   }
 
-  std::sort(result->new_pairs_.begin(), result->new_pairs_.end());
-  auto new_end = std::unique(result->new_pairs_.begin(), result->new_pairs_.end());
-  result->new_pairs_.erase(new_end, result->new_pairs_.end());
-}
+  const Word& v() const {
+    return v_;
+  }
+
+  int task_id() const {
+    return task_id_;
+  }
+
+  struct Stats {
+    int worker_id_ = 0;
+    uint64_t graph_size_ = 0;
+    uint64_t edges_before_reweight_ = 0;
+    uint64_t edges_after_reweight_ = 0;
+    uint64_t count_after_harvest_ = 0;
+    uint64_t count_after_normalize_ = 0;
+  };
+
+  Stats s_;
+  StopwatchesIteration time_;
+  std::vector<std::pair<Word, Word>> generated_pairs_;
+
+ 
+  void InWorkBy(int worker_id) {
+    s_.worker_id_ = worker_id;
+  }
+
+  void Complete() {
+    std::unique_lock<std::mutex> lk(m_computed_);
+    computed_ = true;
+    lk.unlock();
+    cv_computed_.notify_one();
+  }
+
+  void Wait() {
+    std::unique_lock<std::mutex> lk(m_computed_);
+    while (!computed_) {
+      cv_computed_.wait(lk);
+    }
+  }
+
+  void Process() {
+    time_.folding().Click();
+    FoldedGraph2 g;
+    g.PushCycle(u(), g.root(), 1);
+
+    for (auto i = 0u; i < p_.complete_count[v().size()]; ++i) {
+      g.CompleteWith(v());
+    }
+    time_.folding().Click();
+
+    s_.graph_size_ = g.size();
+    s_.edges_before_reweight_ = g.CountNontrivialEdges();
+
+    time_.reweight().Click();
+    g.Reweight();
+    time_.reweight().Click();
+
+    s_.edges_after_reweight_ = g.CountNontrivialEdges();
+
+    time_.harvest().Click();
+    auto eq_u = g.Harvest(p_.max_harvest_length, g.root());
+    time_.harvest().Click();
+
+    s_.count_after_harvest_ = eq_u.size();
+
+    for (auto u_p = eq_u.begin(); u_p != eq_u.end(); ++u_p) {
+      time_.normalize().Click();
+      generated_pairs_.push_back(GetCanonicalPair(v(), *u_p)); 
+      time_.normalize().Click();
+    }
+    std::sort(generated_pairs_.begin(), generated_pairs_.end());
+    auto new_end = std::unique(generated_pairs_.begin(), generated_pairs_.end());
+    generated_pairs_.erase(new_end, generated_pairs_.end());  
+  }
+ private:
+  static int GetNextId() {
+    static int next_task_id = 0;
+    return ++next_task_id;
+  }
+  int task_id_; //!< Number in the queue
+  Word u_;      //!< Generator
+  Word v_;      //!< Relator
+
+  const Parameters& p_; //!< General parameters of the algorithm
+
+  bool computed_ = false;
+  std::condition_variable cv_computed_;
+  mutable std::mutex m_computed_;
+};
+
+template<typename T>
+class shared_queue
+{
+  std::queue<T> queue_;
+  mutable std::mutex m_;
+  std::condition_variable data_cond_;
+
+  shared_queue& operator=(const shared_queue&) = delete;
+  shared_queue(const shared_queue& other) = delete;
+
+public:
+  shared_queue(){}
+
+  void push(T item){
+    std::lock_guard<std::mutex> lock(m_);
+    queue_.push(std::move(item));
+    data_cond_.notify_one();
+  }
+
+  /// \return immediately, with true if successful retrieval
+  bool try_and_pop(T& popped_item){
+    std::lock_guard<std::mutex> lock(m_);
+    if(queue_.empty()){
+      return false;
+    }
+    popped_item = std::move(queue_.front());
+    queue_.pop();
+    return true;
+  }
+
+  /// Try to retrieve, if no items, wait till an item is available and try again
+  bool wait_and_pop(T& popped_item, std::chrono::milliseconds timeout){
+    std::unique_lock<std::mutex> lock(m_); // note: unique_lock is needed for std::condition_variable::wait
+    if (data_cond_.wait_for(lock, timeout, [this](){ return !queue_.empty(); })) {
+      popped_item = std::move(queue_.front());
+      queue_.pop();
+      return true;
+    }
+    return false;
+  }
+
+  bool empty() const{
+    std::lock_guard<std::mutex> lock(m_);
+    return queue_.empty();
+  }
+
+  unsigned size() const{
+    std::lock_guard<std::mutex> lock(m_);
+    return queue_.size();
+  }
+};
+template<typename Task>
+class Worker {
+public:
+  shared_queue<Task> *const task_;
+  bool exit_ = false;
+
+  std::thread my_thread_;
+  int worker_id_;
+ private:
+  Worker(int worker_id, shared_queue<Task> *const task)
+    : task_(task)
+    , worker_id_(worker_id)
+  { }
+
+public:
+  static std::unique_ptr<Worker> Create(shared_queue<Task> *const task) {
+    static int worker_id = 0;
+    std::unique_ptr<Worker> worker(new Worker(++worker_id, task));
+    worker->my_thread_ = std::thread(&Worker::Run, worker.get());
+    return worker;
+  }
+  
+  Worker(const Worker&) = delete;
+
+  void Run() {
+    while (!exit_) {
+      Task t;
+      if (!task_->wait_and_pop(t, std::chrono::milliseconds{100})) {
+        continue;
+      }
+
+      t->InWorkBy(worker_id_);
+      t->Process();
+      t->Complete();
+    }
+  }
+
+  ~Worker() {
+    exit_ = true;
+    my_thread_.join();
+  }
+};
 
 int main(int argc, const char *argv[]) {
   Parameters p;
@@ -251,6 +438,8 @@ int main(int argc, const char *argv[]) {
 
     if (arg.front() == "maxhl") {
       p.max_harvest_length = std::stoi(arg[1]);
+    } else if (arg.front() == "j") {
+      p.workers_count_ = std::stoi(arg[1]);
     } else if (arg.front() == "comp") {
       auto up_to = std::stoi(arg[1]);
       auto count = std::stoi(arg[2]);
@@ -281,6 +470,7 @@ int main(int argc, const char *argv[]) {
   *out << std::setw(9) << ", total";
   *out << std::setw(4) << ", u";
   *out << std::setw(4) << ", v";
+  *out <<                 ", added pairs";
   *out <<                 ", folding time";
   *out <<                 ", harvest time";
   *out << std::endl;
@@ -306,100 +496,100 @@ int main(int argc, const char *argv[]) {
     estats_out << "non-zero edges after reweight, ";
     estats_out << "harvest result size, ";
     estats_out << "after normalize size, ";
+    estats_out << "pairs added count, ";
     estats_out << "u size, v size, ";
+    estats_out << "worker id, ";
     estats_out << "folding time, average folding time, ";
     estats_out << "reweight time, average reweight time, ";
     estats_out << "harvest time, average harvest time, ";
-    estats_out << "normalize time, average normalize time, ";
+    estats_out << "normalize time, average normalize time";
     estats_out << "\n";
   }
 
-  auto initial = Swapped(GetCanonicalPair(p.initial_strings.first.c_str(), p.initial_strings.second.c_str()));
-  auto required = Swapped(GetCanonicalPair(p.required_strings.first.c_str(), p.required_strings.second.c_str()));
+  auto initial = GetCanonicalPair(p.initial_strings.first.c_str(), p.initial_strings.second.c_str());
+  auto required = GetCanonicalPair(p.required_strings.first.c_str(), p.required_strings.second.c_str());
 
   std::set<std::pair<Word, Word>> unprocessed_pairs = {};
   std::set<std::pair<Word, Word>> all_pairs = {};
 
   int counter = 0;
 
-  auto AddPair = [&unprocessed_pairs, &all_pairs, &unproc_words, &counter](const std::pair<Word, Word>& pair) -> bool {
-    auto exists = all_pairs.insert(pair);
-    if (exists.second) {
-      unprocessed_pairs.insert(pair);
-      if (unproc_words.is_open()) {
-        unproc_words << counter << ", ";
-        PrintWord(pair.second, &unproc_words);
-        unproc_words << ", ";
-        PrintWord(pair.first, &unproc_words);
-        unproc_words << "\n";
-      }
-      return true;
-    }
-    return false;
-  };
-
-  AddPair(initial);
-  AddPair(Swapped(initial));
-
+  shared_queue<PairToProcess*> tasks;
   Stopwatches total_time;
+
+  std::vector<std::unique_ptr<Worker<PairToProcess*>>> workers;
+  for (auto i = 0; i < p.workers_count_; ++i) {
+    workers.push_back(Worker<PairToProcess*>::Create(&tasks));
+  }
+
   Stopwatch overall_time;
   auto overall_time_stamp = overall_time.NewIter();
   overall_time_stamp.Click();
 
-  while (!all_pairs.count(required) && !unprocessed_pairs.empty()) {
-    ++counter;
-    *out << std::left << std::setw(7) << counter << ", ";
-    *out << std::right << std::setw(7) << unprocessed_pairs.size() << ", ";
-    *out << std::right << std::setw(7) << all_pairs.size() << ", ";
-
-    if (estats_out.is_open()) {
-      estats_out << counter << ", ";
-      estats_out << unprocessed_pairs.size() << ", ";
-      estats_out << all_pairs.size() << ", ";
+  std::deque<PairToProcess> future_results;
+  auto NewTask = [&future_results, &unproc_words, &tasks, &p, &total_time](const std::pair<Word, Word>& pair, int placed_by) -> int {
+    future_results.emplace_back(pair.first, pair.second, p, &total_time);
+    tasks.push(&future_results.back());
+    if (unproc_words.is_open()) {
+      unproc_words << placed_by << ", ";
+      PrintWord(pair.first, &unproc_words);
+      unproc_words << ", ";
+      PrintWord(pair.second, &unproc_words);
+      unproc_words << "\n";
     }
+    return future_results.back().task_id();
+  };
 
-    Word u, v;
-    auto next_pair = *unprocessed_pairs.begin();
-    unprocessed_pairs.erase(unprocessed_pairs.begin());
-    std::tie(v, u) = std::move(next_pair);
-    *out << std::setw(2) << u.size() << ", ";
-    *out << std::setw(2) << v.size() << ", ";
-
-    if (proc_words.is_open()) {
-      proc_words << counter << ", ";
-      PrintWord(u, &proc_words);
-      proc_words << ", ";
-      PrintWord(v, &proc_words);
-      proc_words << "\n";
+  auto AddPair = [&NewTask, &all_pairs, &unproc_words, &counter](const std::pair<Word, Word>& pair, int added_by) -> int {
+    auto exists = all_pairs.insert(pair);
+    if (exists.second) {
+      return NewTask(pair, added_by);
     }
+    return false;
+  };
 
-    Result result(u, v, &total_time);
-    ProceedPair(u, v, p, &result);
+  auto initial_task_id = AddPair(initial, 0);
+  AddPair(Swapped(initial), initial_task_id);
 
-    if (estats_out.is_open()) {
-      estats_out << result.estats_out_.str();
-    }
+  while (!all_pairs.count(required) && !future_results.empty()) {
+    future_results.front().Wait();
+
+    auto& next_result = future_results.front();
 
     auto pairs_count_before = all_pairs.size();
 
-    for (auto&& new_pair : result.new_pairs_) {
-      if (AddPair(Swapped(new_pair))) {
-        AddPair(new_pair);
+    for (auto&& new_pair : next_result.generated_pairs_) {
+      auto new_id = AddPair(new_pair, next_result.task_id());
+      if (new_id) {
+        AddPair(Swapped(new_pair), new_id);
       }
     }
 
-    if (estats_out.is_open()) {
-      estats_out << all_pairs.size() - pairs_count_before << ", ";
-      estats_out << u.size() << ", ";
-      estats_out << v.size() << ", ";
-    }
+    auto pairs_added = all_pairs.size() - pairs_count_before;
+    next_result.time_.Reset();
 
-    result.time_.Reset();
-
+    *out << std::left << std::setw(7) << next_result.task_id() << ", ";
+    *out << std::right << std::setw(7) << pairs_count_before - next_result.task_id() << ", ";
+    *out << std::right << std::setw(7) << pairs_count_before << ", ";
+    *out << std::setw(2) << next_result.u().size() << ", ";
+    *out << std::setw(2) << next_result.v().size() << ", ";
+    *out << pairs_added << ", ";
     *out << total_time.folding().last() << ", ";
     *out << total_time.harvest().last() << std::endl;
 
     if (estats_out.is_open()) {
+      estats_out << next_result.task_id() << ", ";
+      estats_out << pairs_count_before - next_result.task_id() << ", ";
+      estats_out << pairs_count_before << ", ";
+      estats_out << next_result.s_.graph_size_ << ", ";
+      estats_out << next_result.s_.edges_before_reweight_ << ", ";
+      estats_out << next_result.s_.edges_after_reweight_ << ", ";
+      estats_out << next_result.s_.count_after_harvest_ << ", ";
+      estats_out << next_result.s_.count_after_normalize_ << ", ";
+      estats_out << pairs_added << ", ";
+      estats_out << next_result.u().size() << ", ";
+      estats_out << next_result.v().size() << ", ";
+      estats_out << next_result.s_.worker_id_ << ", ";
       estats_out << total_time.folding().last() << ", ";
       estats_out << total_time.folding().average() << ", ";
       estats_out << total_time.reweight().last() << ", ";
@@ -410,10 +600,20 @@ int main(int argc, const char *argv[]) {
       estats_out << total_time.normalize().average() << "\n";
     }
 
+    if (proc_words.is_open()) {
+      proc_words << next_result.task_id() << ", ";
+      PrintWord(next_result.u(), &proc_words);
+      proc_words << ", ";
+      PrintWord(next_result.v(), &proc_words);
+      proc_words << "\n";
+    }
+
     out->flush();
     estats_out.flush();
     proc_words.flush();
     unproc_words.flush();
+
+    future_results.pop_front();
   }
   overall_time_stamp.Click();
   overall_time_stamp.Reset();
@@ -421,7 +621,6 @@ int main(int argc, const char *argv[]) {
   *out << "total time: " << overall_time.total_time() << std::endl;
   *out << "total rest(av): " << average_other_time << std::endl;
   *out << "total iterations: " << total_time.folding().iterations() << std::endl;
-
 
   return 0;
 }
